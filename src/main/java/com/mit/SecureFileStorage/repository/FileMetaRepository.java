681f24904959f0f2cc4b46a68b5cca2c6f1da44a
Suma <109804241+NaikSuma@users.noreply.github.com>
2026-09-22 21:51:36 +0530

    Update FileMetaRepository.java

--- src/main/java/com/mit/SecureFileStorage/repository/FileMetaRepository.java
+++ src/main/java/com/mit/SecureFileStorage/repository/FileMetaRepository.java
@@ -1,4 +1,4 @@
-package com.mit.SecureFileStorage.repository;
+package com.mit.SecureFileStorage.repository; 
  
 import com.mit.SecureFileStorage.Model.FileMeta;
 import org.springframework.data.jpa.repository.JpaRepository; 
